094a8a81ae1b4bdfee80323f1a8fb44fc62a61e1
pando <pandopro@yandex.ru>
2020-06-23 18:07:20 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Boolean answer = bean1 == bean2;
+        System.out.println(bean1.getMessage());
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+        Boolean answer2 = beanCat1 == beanCat2;
+        System.out.println("1. helloWorld1 == helloworld2:  " + answer);
+        System.out.println("1. cat1 == cat2:  " + answer2);
+
     }
 }
